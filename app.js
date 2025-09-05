be67b28c2d4a9dbefbe02bf79600549ef650673a
andwar92 <andwar1992@gmail.com>
2025-09-05 10:50:06 +0200

    Lagt til filer

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = document.getElementById('app');
+
+updateView();
+function updateView(){
+    app.innerHTML = /*HTML*/` 
+    <div id="tower-1" class="tower">
+      <div class="rod"></div>
+      <div class="disk disk-3"></div>
+      <div class="disk disk-2"></div>
+      <div class="disk disk-1"></div>
+    </div>
+
+    <div id="tower-2" class="tower">
+      <div class="rod"></div>
+    </div>
+
+    <div id="tower-3" class="tower">
+      <div class="rod"></div>
+    </div>
+
+    `;
+};
