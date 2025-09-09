ab2fccd5f7944792ee043065f79931364dbe3339
andwar92 <andwar1992@gmail.com>
2025-09-09 12:41:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const app = document.getElementById('app');
-let tower2 = '';
+let disks = [
+  document.getElementById("disk-1"),
+  document.getElementById("disk-2"),
+  document.getElementById("disk-3")
+  ];
+  let towers = [
+    document.getElementById("tower-1"),
+    document.getElementById("tower-2"),
+    document.getElementById("tower-3")
+  ]
 updateView();
 function updateView() {
     app.innerHTML = /*HTML*/` 
     <div id="tower-1" class="tower">
       <div class="rod"></div>
-      <div class="disk disk-3"></div>
-      <div class="disk disk-2"></div>
-      <div onclick="moveDisc()" class="disk disk-1"></div>
+      <div id="disc-3" onclick="transfer(this)" class="disk disk-3"></div>
+      <div id="disc-2" onclick="transfer(this)" class="disk disk-2"></div>
+      <div id="disc-1" onclick="transfer(this)" class="disk disk-1"></div>
     </div>
       
     <div id="tower-2" class="tower">
@@ -21,7 +30,19 @@ function updateView() {
     `;
 };
 
-function moveDisc(){
-  document.getElementById('app').innerHTML += 
-  tower2 = ` <div onclick="moveDisc()" class="disk disk-1"></div>`;
+function transfer(disk){
+
+  if (disk.parentElement.id === "tower-1" + "") {
+    towers[1].appendChild(disk);
+  }
+
+  // if (disk.parentElement.id === "tower-1" ) {
+  //   towers[1].appendChild(disk);
+    
+  // }
+  // else if (disk.parentElement.id === "tower-2") {
+  //   towers[2].appendChild(disk);
+  // }
+  // else towers[0].appendChild(disk);
+  // return ;
 };
