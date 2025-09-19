0bac759b1776cfb706ac74d6a86f73e3dbcd7d0d
andwar92 <andwar1992@gmail.com>
2025-09-19 09:25:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
+//Model
 const app = document.getElementById('app');
-let disks = [
-  document.getElementById("disk-1"),
-  document.getElementById("disk-2"),
-  document.getElementById("disk-3")
-  ];
-  let towers = [
-    document.getElementById("tower-1"),
-    document.getElementById("tower-2"),
-    document.getElementById("tower-3")
-  ]
+const towerOfHanoi = {
+  towers: {
+    tower1: "tower-1",
+    tower2: "tower-2",
+    tower3: "tower-3",
+  },
+
+  discs: {
+    disk1: "tower-1",
+    disk2: "tower-2",
+    disk3: "tower-3",
+  },
+}
+//Veiw
 updateView();
 function updateView() {
-    app.innerHTML = /*HTML*/` 
-    <div id="tower-1" class="tower">
+ app.innerHTML= drawTowerOfHanoi("tower-1","disc-1","disk disk-3");
+};
+
+function drawTowerOfHanoi(tower,disk,big) {
+  let html = /*HTML*/`
+    <div id="${tower}" class="tower">
       <div class="rod"></div>
-      <div id="disc-3" onclick="transfer(this)" class="disk disk-3"></div>
-      <div id="disc-2" onclick="transfer(this)" class="disk disk-2"></div>
-      <div id="disc-1" onclick="transfer(this)" class="disk disk-1"></div>
+      <div id="${disk}" onclick="transfer(this)" class="${big}"></div>
+      <div id="${disk}" onclick="transfer(this)" class="disk disk-2"></div>
+      <div id="${disk}" onclick="transfer(this)" class="disk disk-1"></div>
     </div>
       
     <div id="tower-2" class="tower">
@@ -26,23 +35,27 @@ function updateView() {
     <div id="tower-3" class="tower">
       <div class="rod"></div>
     </div>
-
-    `;
+`;
+  return html;
 };
 
-function transfer(disk){
-
-  if (disk.parentElement.id === "tower-1" + "") {
-    towers[1].appendChild(disk);
+function getDisc(id) {
+  return towerOfHanoi.discs.find(element => element.id === id)
+};
+function getTower(id) {
+  return towerOfHanoi.towers.find(element => element.id === id);
+};
+function transfer(diskElement) {
+  let id = diskElement.id;
+  let clickedDisc = getDisc(id);
+  let tower
+  if (clickedDisc.tower === "tower-1") {
+    clickedDisc = "tower-2"; updateView();
   }
-
-  // if (disk.parentElement.id === "tower-1" ) {
-  //   towers[1].appendChild(disk);
-    
-  // }
+  console.log(clickedDisc);
   // else if (disk.parentElement.id === "tower-2") {
   //   towers[2].appendChild(disk);
   // }
   // else towers[0].appendChild(disk);
-  // return ;
+  return;
 };
